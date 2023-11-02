d625ba4eb8af018922e380a9d3c1351ac1296db6
dominikn <dominik.nwk@gmail.com>
2023-11-02 13:28:45 +0100

    working demo for host shm communication

--- demo/auto/host/shm/run_client.sh
+++ demo/auto/host/shm/run_client.sh
@@ -9,6 +9,7 @@ trap cleanup EXIT
 docker compose -f compose.client.yaml up -d
 
 export ROS_LOCALHOST_ONLY=1
+sudo ip link set lo multicast on
 
 # Run ROS2 listener
 ros2 run demo_nodes_cpp listener
