e7063d9e60c7bffad581d1727986653510bfaf41
dominikn <dominik.nwk@gmail.com>
2023-11-02 13:45:45 +0100

    all reference configs for docker and for host work fine

--- demo/auto/host/shm-advanced/run_server.sh
+++ demo/auto/host/shm-advanced/run_server.sh
@@ -10,8 +10,7 @@ export MY_UID=$(id -u)
 export MY_GID=$(id -g)
 docker compose -f compose.server.yaml up -d
 
-export ROS_LOCALHOST_ONLY=1
-sudo ip link set lo multicast on
+export FASTRTPS_DEFAULT_PROFILES_FILE=$(pwd)/localhost-shm-only.xml
 
 # Run ROS2 listener
 ros2 run demo_nodes_cpp talker
